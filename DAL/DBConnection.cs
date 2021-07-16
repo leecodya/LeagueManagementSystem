486127d005c3f8b4135f4dab0cf0fb1107ec369b
leecodya <lee.codya@gmail.com>
2021-07-15 20:06:29 -0500

    added dummy data to database and created a class that would handle the db connection

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace LeageManagementSystem.DAL
+{
+    /// <summary>
+    /// Class to establish a connection directly to the DB (LeagueDatabase)
+    /// </summary>
+    class DBConnection
+    {
+        /// <summary>
+        /// Method to establish a connection to the League Management DB
+        /// </summary>
+        /// <returns>Returns the connection to the database</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog= LeageDatabase;" + 
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ SQL Scripts/InsertDummyData.sql
@@ -0,0 +1,18 @@
+insert into League
+values
+('HCDGC Spring  2021', '2021-03-25', '2021-05-13', 'Central Pines');
+
+insert into Player
+values
+('Cody', 'Lee', '157978'),
+('LeighAnn', 'Shell', ''),
+('Jimmy', 'Nicholson', '123456');
+
+insert into LMSUser
+values
+('clee', 'test1234', 'admin', 1),
+('jnicholson', 'test1234', 'regular', 3);
+
+insert into Round
+values
+(1, 1, '2021-07-15', 54);
