bc33d3542ca9314d9b435737f1084235c2908ccc
leecodya <lee.codya@gmail.com>
2021-08-05 13:39:21 -0500

    created Add round scored user control and added ui components with no functionality. insterted this user control into the guest dashboards tab control

---
+++ UserControls/AddRoundScoredUserControl.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LeageManagementSystem.UserControls
+{
+    partial class AddRoundScoredUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.leagueLabel = new System.Windows.Forms.Label();
+            this.leagueComboBox = new System.Windows.Forms.ComboBox();
+            this.playerComboBox = new System.Windows.Forms.ComboBox();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.dateOfRoundTextBox = new System.Windows.Forms.TextBox();
+            this.scoreTextBox = new System.Windows.Forms.TextBox();
+            this.addRoundScoredButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(233, 14);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(178, 25);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Add Round Scored";
+            // 
+            // leagueLabel
+            // 
+            this.leagueLabel.AutoSize = true;
+            this.leagueLabel.Location = new System.Drawing.Point(198, 71);
+            this.leagueLabel.Name = "leagueLabel";
+            this.leagueLabel.Size = new System.Drawing.Size(65, 17);
+            this.leagueLabel.TabIndex = 1;
+            this.leagueLabel.Text = "*League:";
+            // 
+            // leagueComboBox
+            // 
+            this.leagueComboBox.FormattingEnabled = true;
+            this.leagueComboBox.Location = new System.Drawing.Point(271, 68);
+            this.leagueComboBox.Name = "leagueComboBox";
+            this.leagueComboBox.Size = new System.Drawing.Size(184, 24);
+            this.leagueComboBox.TabIndex = 2;
+            // 
+            // playerComboBox
+            // 
+            this.playerComboBox.FormattingEnabled = true;
+            this.playerComboBox.Location = new System.Drawing.Point(271, 117);
+            this.playerComboBox.Name = "playerComboBox";
+            this.playerComboBox.Size = new System.Drawing.Size(184, 24);
+            this.playerComboBox.TabIndex = 4;
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Location = new System.Drawing.Point(206, 120);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(57, 17);
+            this.playerLabel.TabIndex = 3;
+            this.playerLabel.Text = "*Player:";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(154, 169);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(109, 17);
+            this.dateLabel.TabIndex = 5;
+            this.dateLabel.Text = "*Date of Round:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(209, 216);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(54, 17);
+            this.scoreLabel.TabIndex = 7;
+            this.scoreLabel.Text = "*Score:";
+            // 
+            // dateOfRoundTextBox
+            // 
+            this.dateOfRoundTextBox.Location = new System.Drawing.Point(271, 166);
+            this.dateOfRoundTextBox.Name = "dateOfRoundTextBox";
+            this.dateOfRoundTextBox.Size = new System.Drawing.Size(148, 22);
+            this.dateOfRoundTextBox.TabIndex = 8;
+            // 
+            // scoreTextBox
+            // 
+            this.scoreTextBox.Location = new System.Drawing.Point(271, 213);
+            this.scoreTextBox.Name = "scoreTextBox";
+            this.scoreTextBox.Size = new System.Drawing.Size(73, 22);
+            this.scoreTextBox.TabIndex = 9;
+            // 
+            // addRoundScoredButton
+            // 
+            this.addRoundScoredButton.Location = new System.Drawing.Point(271, 266);
+            this.addRoundScoredButton.Name = "addRoundScoredButton";
+            this.addRoundScoredButton.Size = new System.Drawing.Size(94, 28);
+            this.addRoundScoredButton.TabIndex = 10;
+            this.addRoundScoredButton.Text = "Add";
+            this.addRoundScoredButton.UseVisualStyleBackColor = true;
+            // 
+            // AddRoundScoredUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.addRoundScoredButton);
+            this.Controls.Add(this.scoreTextBox);
+            this.Controls.Add(this.dateOfRoundTextBox);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.playerComboBox);
+            this.Controls.Add(this.playerLabel);
+            this.Controls.Add(this.leagueComboBox);
+            this.Controls.Add(this.leagueLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "AddRoundScoredUserControl";
+            this.Size = new System.Drawing.Size(628, 438);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label leagueLabel;
+        private System.Windows.Forms.ComboBox leagueComboBox;
+        private System.Windows.Forms.ComboBox playerComboBox;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.TextBox dateOfRoundTextBox;
+        private System.Windows.Forms.TextBox scoreTextBox;
+        private System.Windows.Forms.Button addRoundScoredButton;
+    }
+}
--- View/GuestDashboard.Designer.cs
+++ View/GuestDashboard.Designer.cs
@@ -28,23 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.addRoundScoredTabPage = new System.Windows.Forms.TabPage();
+            this.guestTabControl = new System.Windows.Forms.TabControl();
+            this.addRoundScoredUserControl1 = new LeageManagementSystem.UserControls.AddRoundScoredUserControl();
+            this.addRoundScoredTabPage.SuspendLayout();
+            this.guestTabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(738, 45);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(200, 29);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Guest Dashboard";
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(40, 45);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(130, 20);
+            this.welcomeLabel.TabIndex = 1;
+            this.welcomeLabel.Text = "Welcome guest!";
+            // 
+            // addRoundScoredTabPage
+            // 
+            this.addRoundScoredTabPage.Controls.Add(this.addRoundScoredUserControl1);
+            this.addRoundScoredTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addRoundScoredTabPage.Location = new System.Drawing.Point(4, 27);
+            this.addRoundScoredTabPage.Name = "addRoundScoredTabPage";
+            this.addRoundScoredTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.addRoundScoredTabPage.Size = new System.Drawing.Size(1012, 421);
+            this.addRoundScoredTabPage.TabIndex = 0;
+            this.addRoundScoredTabPage.Text = "Add Round Scored";
+            this.addRoundScoredTabPage.UseVisualStyleBackColor = true;
+            // 
+            // guestTabControl
+            // 
+            this.guestTabControl.Controls.Add(this.addRoundScoredTabPage);
+            this.guestTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guestTabControl.Location = new System.Drawing.Point(1, 104);
+            this.guestTabControl.Name = "guestTabControl";
+            this.guestTabControl.SelectedIndex = 0;
+            this.guestTabControl.Size = new System.Drawing.Size(1020, 452);
+            this.guestTabControl.TabIndex = 2;
+            // 
+            // addRoundScoredUserControl1
+            // 
+            this.addRoundScoredUserControl1.Location = new System.Drawing.Point(138, 6);
+            this.addRoundScoredUserControl1.Name = "addRoundScoredUserControl1";
+            this.addRoundScoredUserControl1.Size = new System.Drawing.Size(628, 438);
+            this.addRoundScoredUserControl1.TabIndex = 0;
             // 
             // GuestDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1022, 558);
+            this.Controls.Add(this.guestTabControl);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.titleLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GuestDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Guest Dashboard";
+            this.addRoundScoredTabPage.ResumeLayout(false);
+            this.guestTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.TabPage addRoundScoredTabPage;
+        private System.Windows.Forms.TabControl guestTabControl;
+        private UserControls.AddRoundScoredUserControl addRoundScoredUserControl1;
     }
 }
